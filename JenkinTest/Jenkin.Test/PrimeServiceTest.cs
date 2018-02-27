e3b9320a571687cb27c3b04e1fa99de2855bc6bb
Tri Nguyen <valentino.nguyen.92@gmail.com>
2018-02-27 11:21:07 +0700

    init jenkin test project

---
+++ JenkinTest/Jenkin.Test/PrimeServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using JenkinService;
+using System;
+using Xunit;
+
+namespace Jenkin.Test
+{
+    public class PrimeServiceTest
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeServiceTest()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Fact]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+        }
+    }
+}
---
+++ JenkinTest/JenkinTest/PrimeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JenkinService
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate == 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
